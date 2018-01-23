b5e43b41a2ac5b2bd6ca5d4b62bf111476795358
Patrick Hansen <patrick.hansen@hotmail.com>
2018-01-23 19:51:10 +0100

    Added banner elements unique id

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
 
     var banner = $("#banner");
 
+    function elementId()
+    {
+        var highestId = 0;
+        $(".banner-element").each(function(){
+            highestId = parseInt($(this).attr('data-element-id'));
+        });
+        var nextId = highestId + 1;
+        return nextId;
+    }
+
     /****Settings */
     $("#show-grid-lines").on('click',function(){
         if($(this).is(':checked')){
@@ -27,19 +37,20 @@ $(document).ready(function(){
     $("#add_text").on('click',function(){
         switch($("#txt_type").val()){
             case "p":
-                banner.append("<p class='dragger draggable ui-widget-content'>"+$("#txt").val()+"</p>");
+                banner.append("<p class='dragger draggable ui-widget-content banner-element' data-element-id='"+elementId()+"'>"+$("#txt").val()+"</p>");
                 $('.draggable').draggable({containment:"#banner"});
                 break;
             case "h1":
-                banner.append("<h1 class='dragger draggable ui-widget-content'>"+$("#txt").val()+"</h1>");
+                banner.append("<h1 class='dragger draggable ui-widget-content banner-element' data-element-id='"+elementId()+"'>"+$("#txt").val()+"</h1>");
                 $('.draggable').draggable({containment:"#banner"});
                 break;
             case "h2":
-                banner.append("<h2 class='dragger draggable ui-widget-content'>"+$("#txt").val()+"</h2>");
+                banner.append("<h2 class='dragger draggable ui-widget-content banner-element' data-element-id='"+elementId()+"'>"+$("#txt").val()+"</h2>");
                 $('.draggable').draggable({containment:"#banner"});
                 break;
         }
         $("#txt").val("");
+       
     });
 
     $("#banner").children().each(function(){
